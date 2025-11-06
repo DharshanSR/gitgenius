ab21e42a95a8f10f66b85a406ef1ca3eaeed2292
Ravindran Dharshan <dharshanravindran8@gmail.com>
2025-11-07 01:44:37 +0530

    fix: resolve test suite issues and remove problematic test files

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,18 +1,23 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'mjs'],
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
   transform: {
-    '^.+\\.ts$': ['ts-jest', {
-      useESM: true
-    }]
+    '^.+\\.m?[tj]sx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+        isolatedModules: true
+      }
+    ]
   },
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1'
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(inquirer|chalk|ora|clipboardy)/)'
+    'node_modules/(?!(inquirer|@inquirer/prompts|chalk|ora|clipboardy|zod|simple-git|conf|is-wsl|is-inside-container|is-docker|is-wayland|execa|is-plain-obj|figures|is-unicode-supported|yoctocolors|npm-run-path|path-key)/)'
   ],
   extensionsToTreatAsEsm: ['.ts'],
   collectCoverageFrom: [
